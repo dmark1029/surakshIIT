3fd59ea6c4924c22914548080fb10b7e41e913b1
Udit <uditpd3000@gmail.com>
2022-03-01 13:10:42 +0530

    Frontend initialised

--- Frontend/src/components/Sidebar/Sidebar.js
+++ Frontend/src/components/Sidebar/Sidebar.js
@@ -236,7 +236,36 @@ const Sidebar = (props) => {
           {/* Divider */}
           <hr className="my-3" />
           {/* Heading */}
-          
+          <h6 className="navbar-heading text-muted">Documentation</h6>
+          {/* Navigation */}
+          <Nav className="mb-md-3" navbar>
+            <NavItem>
+              <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/overview?ref=adr-admin-sidebar">
+                <i className="ni ni-spaceship" />
+                Getting started
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/colors?ref=adr-admin-sidebar">
+                <i className="ni ni-palette" />
+                Foundation
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/alerts?ref=adr-admin-sidebar">
+                <i className="ni ni-ui-04" />
+                Components
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <Nav className="mb-md-3" navbar>
+            <NavItem className="active-pro active">
+              <NavLink href="https://www.creative-tim.com/product/argon-dashboard-pro-react?ref=adr-admin-sidebar">
+                <i className="ni ni-spaceship" />
+                Upgrade to PRO
+              </NavLink>
+            </NavItem>
+          </Nav>
         </Collapse>
       </Container>
     </Navbar>
