10c7c7d542fa7e79961edf7c726d9ab0a8efa707
aarchie-r <aarchi20@iitk.ac.in>
2022-03-04 01:40:48 +0530

    user_update

--- Backend/user/models.py
+++ Backend/user/models.py
@@ -3,10 +3,20 @@ from django.db import models
 class Security(models.Model):
     name= models.CharField(max_length=100)
     uid = models.CharField(max_length=100)
+    email = models.EmailField(max_length=256)
+    phone = models.CharField(max_length=15)
+    dp = models.ImageField(upload_to='pic', blank=True)
 
 class Residents(models.Model):
     name= models.CharField(max_length=100)
     uid = models.CharField(max_length=100)
+    email = models.EmailField(max_length=256)
+    phone = models.CharField(max_length=15)
+    room_no = models.CharField(max_length=256)
+    address = models.CharField(max_length=256)
+    gender = models.CharField(max_length=20)
+    role = models.CharField(max_length=256)
+    dp = models.ImageField(upload_to='pic', blank=True)
 
 
 # Create your models here.
